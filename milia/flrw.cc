67e3b171201df16bc340c050d3cbff757cd1bac4
Sergio Pascual <sergiopr@fis.ucm.es>
2008-08-06 20:54:24 +0000

    Distances and times working

--- milia/flrw.cc
+++ milia/flrw.cc
@@ -22,39 +22,127 @@
 
 #include "flrw.h"
 #include "exception.h"
+#include <cstdlib>
+#include <gsl/gsl_math.h>
 
 namespace milia {
 
 namespace metrics {
 
+const double flrw::ms_hubble_radius = 2.99792e5; // Hubble Radius in Mpc
+const double flrw::ms_hubble_time = 9.78e2; // Hubble time in Gyr
+
+
 flrw::flrw(double hubble, double matter, double lambda) :
-	m_hu(hubble), m_om(matter), m_ol(lambda)
+	m_hu(hubble), m_om(matter), m_ol(lambda), m_ok(1.0 - m_om - m_ol),
+			m_kap(GSL_SIGN(m_ok))
 
 {
+	// hubble <= 0
+	if (m_hu <= 0)
+		throw milia::exception();
 	// om < 0
 	if (m_om < 0)
 		throw milia::exception();
 	//ol < 0
 	if (m_ol < 0)
 		throw milia::exception();
+
+	m_b = (-13.5 * gsl_pow_2(m_om) * m_ol / (gsl_pow_3(m_ok)));
+
+	if (!is_there_big_bang(m_om, m_ol, m_b)) {
+		throw milia::exception();
+	}
+	m_case = calculation_case(m_om, m_ol, m_ok, m_b);
+
+	m_r_h = ms_hubble_radius / m_hu;
+	m_t_h = ms_hubble_time / m_hu;
+	m_universe_age = age();
 }
 
-double flrw::distance_luminosity(double redshift) const {
-	return 0;
+bool flrw::is_there_big_bang(double matter, double lambda, double b) {
+	// Check if there is Big Bang
+
+	// There isn't in two cases:
+	// OM=0 && OL>1
+	if (matter == 0 && lambda >= 1)
+		return false;
+	// OM>0 && OL>=1 && B <= 2
+	if (matter> 0 && lambda >= 1 && b <= 2)
+		return false;
+	return true;
+
 }
 
-double flrw::distance_comoving(double redshift) const {
-	return 0;
+flrw::CALC_CASES flrw::calculation_case(double matter, double lambda,
+		double curvature, double b) {
+	const bool l3 = (matter <= 0);
+	const bool l4 = (lambda <= 0);
+	// om=ol=0
+	if (l3 && l4)
+		return OM_OL_0;
+	// ol=0 0<om<1
+	if (l4 && matter < 1)
+		return OL_1;
+	// ol=0 om>1
+	if (l4 && matter> 1)
+		return OL_2;
+	// ol=0 om==1
+	if (l4)
+		return OL_3;
+	// om=0
+	if (l3)
+		return OM;
+	// om+ol==1
+	if (curvature <= 0)
+		return OM_OL_1;
+	// om+ol!=1
+	// a1 b<0 || b>2
+	if ((b <= 0) ||(b> 2))
+		return A1;
+	// a2 b=2
+	if (b == 2)
+		return A2_1;
+	// a2 0<b<2
+	if ((b> 0) && (b < 2))
+		return A2_2;
+	// Fallback
+	return NO_CASE;
+
 }
 
-double flrw::distance_angular(double redshift) const {
-	return 0;
+double flrw::helper(double z) const {
+	return sqrt(m_om*gsl_pow_3(1+z)+m_ok*gsl_pow_2(1+z)+m_ol);
+	// No se cual de estas dos es mas rapida
+	// return sqrt(gsl_pow_2(1.+z)*(1.+m_om*z)-z*m_ol*(2.+z))));
 }
 
-double flrw::time_lookback(double redshift) const {
-	return 0;
+double flrw::distance_comoving(double z) const {
+	const double dm = distance_comoving_transverse(z);
+	if (m_ok == 0)
+		return dm;
+	else {
+		const double sqok = sqrt(abs(m_ok));
+		if (m_ok > 0)
+			return m_r_h / sqok * asinh(dm * sqok / m_r_h);
+		else
+			return m_r_h / sqok * asin(dm * sqok / m_r_h);
+	}
+}
+
+double flrw::distance_comoving_transverse(double z) const {
+	return distance_luminosity(z)/(1. + z);
+}
+
+double flrw::distance_angular(double z) const {
+	return distance_luminosity(z)/gsl_pow_2(1. + z);
+}
+
+double flrw::lookback_time(double z) const {
+	return m_universe_age - age(z);
 }
 
 } // namespace metrics
 
-} // namespace milia
+}
+// namespace milia
--- milia/flrw.h
+++ milia/flrw.h
@@ -29,16 +29,59 @@ namespace metrics {
 
 /**
  * The Friedmann-Lemaître-Robertson-Walker metric
+ * 
+ * This class computes the common cosmological distances and times.
+ * It uses elliptical functions from gsl.
+ * It is based on the paper <a href="http://xxx.unizar.es/abs/astro-ph/9905116">%astro-ph/9905116</a> for the relations between distances.
+ * The age is computed from <a href="http://xxx.unizar.es/abs/astro-ph/0003463">%astro-ph/0003463</a> with elliptical functions.
+ * Distances are computed from the luminosity distance using<a href="http://xxx.unizar.es/abs/astro-ph/0002334">%astro-ph/0002334</a> without inhomogeneities.
+ * 
+ * 
  */
 class flrw {
 public:
 	flrw(double hubble_constant, double matter_density, double lambda_density);
 
 	double distance_luminosity(double redshift) const;
-	double distance_comoving(double redshift) const;
-	double distance_angular(double redshift) const;
-	double time_lookback(double redshift) const;
 
+	/**
+	 * Comoving distance (line of sight)
+	 * \f[
+	 * dc(z)=\frac{1}{H_0}\int_0^z \frac{dt}{\sqrt{\Omega_M(1+t)^3+\Omega_k(1+t)^2+\Omega_{\Lambda}}}
+	 * \f]
+	 * 
+	 */
+	double distance_comoving(double redshift) const;
+
+	/**
+	 * Comoving distance (transverse)
+	 */
+	double distance_comoving_transverse(double redshift) const;
+
+	/**
+	 * Angular distance
+	 */
+	double distance_angular(double redshift) const;
+
+	/**
+	 * Look-back time
+	 */
+	double lookback_time(double redshift) const;
+
+	/**
+	 * Age of the Universe back to a given redshift 
+	 */
+	double age(double redshift) const;
+
+	/**
+	 * Age of the Universe 
+	 */
+	double age() const;
+	
+	/**
+	 * Helper function
+	 */
+	double helper(double redshift) const;
 private:
 	//!Hubble radius
 	static const double ms_hubble_radius;
@@ -49,8 +92,72 @@ private:
 	double m_hu;
 	//!Matter density
 	double m_om;
-	//!cosmologyologycal constant density
+	//!Cosmological constant density
 	double m_ol;
+
+	/**
+	 * Curvature parameter
+	 */
+	double m_ok;
+
+	/**
+	 * Sign of the curvature parameter
+	 */
+	int m_kap;
+
+	/**
+	 * Separation parameter
+	 */
+	double m_b;
+
+	//! Scale size of the Universe
+	double m_r_h;
+	//! Scale time of the Universe
+	double m_t_h;
+	//! Age of the Universe
+	double m_universe_age;
+
+	/**
+	 *  Check if there is Big Bang
+	 */
+	static bool is_there_big_bang(double matter, double lambda, double b);
+
+	/** 
+	 * Different casses of the computation
+	 */
+	enum CALC_CASES {
+		OM_OL_0=1, //om=ol=0
+		OL_1, //ol=0 0<om<1
+		OL_2, //ol=0 om>1
+		OL_3, //ol=0 om=1
+		OM, //om=0
+		A1, //om+ol!=1 b<0 || b>2
+		A2_1, //om+ol!=1 b=2
+		A2_2, //om+ol!=1 0<b<2
+		OM_OL_1, //om+ol=1
+		NO_CASE=0,
+	};
+
+	/**
+	 * Selecting our case of the computation
+	 */
+	static CALC_CASES calculation_case(double matter, double lambda,
+			double curvature, double b);
+
+	/**
+	 * Our computation case
+	 */
+	CALC_CASES m_case;
+
+	/**
+	 * Time auxiliary functions
+	 */
+	double tolz(double z) const;
+	double tomz(double z) const;
+	double ta1(double z) const;
+	double ta2(double z) const;
+	double tb(double z) const;
+	double integrate_time(double) const;
 
 };
 
@@ -59,4 +166,4 @@ private:
 } // namespace milia
 
 
-#endif /* _MILIA_METRIC_H_ */
+#endif /* _MILIA_FLRW_H_ */
