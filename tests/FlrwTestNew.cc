548b30020e0c0b9ee710ef3eccaac6bb901833c2
Sergio Pascual <sergiopr@fis.ucm.es>
2013-06-03 16:41:31 +0200

    Testing for new style implementation

---
+++ tests/FlrwTestNew.cc
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2008-2013 Sergio Pascual
+ *
+ * This file is part of Milia
+ *
+ * Milia is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Milia is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Milia.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "FlrwTestNew.h"
+#include "milia/metric.h"
+
+// Registers the fixture into the 'registry'
+CPPUNIT_TEST_SUITE_REGISTRATION(FlrwTestNew);
+
+void FlrwTestNew::setUp() {
+}
+
+void FlrwTestNew::tearDown() {
+}
+
---
+++ tests/FlrwTestNew.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2008-2013 Sergio Pascual
+ *
+ * This file is part of Milia
+ *
+ * Milia is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Milia is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Milia.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef MILIA_FLRW_TEST_NEW_H
+#define MILIA_FLRW_TEST_NEW_H
+
+#include "FlrwTestData.h"
+#include <stdexcept>
+#include <cppunit/extensions/HelperMacros.h>
+
+class FlrwTestNew : public CppUnit::TestFixture, public FlrwTestData
+{
+    CPPUNIT_TEST_SUITE(FlrwTestNew);
+    CPPUNIT_TEST_SUITE_END();
+public:
+    void setUp();
+
+    void tearDown();
+
+};
+
+
+#endif // MILIA_FLRW_TEST_NEW_H
